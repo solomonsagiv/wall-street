4de1ca2c8fb52177a62500f10d9319e31a82b3e7
sagiv <solomon.sagiv12@gmail.com>
2020-02-09 09:44:59 +0200

    eeee

--- src/main/java/api/Test.java
+++ src/main/java/api/Test.java
@@ -1,11 +1,8 @@
 package api;
 
-import com.mysql.jdbc.Connection;
-import com.mysql.jdbc.Statement;
-
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @EnableAsync
 public class Test {
@@ -13,29 +10,54 @@ public class Test {
 
     public static void main( String[] args ) throws SQLException {
 
-        String url = "jdbc:mysql://parisdb.chuxlqcvlex2.eu-west-3.rds.amazonaws.com:3306/";
-        String userName = "sagivMasterUser";
-        String password = "Solomonsagivawsmaster12";
-        String dbName = "stocks";
-        String driver = "com.mysql.jdbc.Driver";
-        Connection connection = ( Connection ) DriverManager.getConnection( url + dbName, userName, password );
+//        String url = "jdbc:mysql://parisdb.chuxlqcvlex2.eu-west-3.rds.amazonaws.com:3306/";
+//        String userName = "sagivMasterUser";
+//        String password = "Solomonsagivawsmaster12";
+//        String dbName = "stocks";
+//        String driver = "com.mysql.jdbc.Driver";
+//        Connection connection = ( Connection ) DriverManager.getConnection( url + dbName, userName, password );
+//
+//        Statement stmt = ( Statement ) connection.createStatement( );
+//
+//        String query = "INSERT INTO `stocks`.`spx_daily` (`id`, `date`, `exp_name`, `open`, `high`, `low`, `close`, `con_up`, `con_down`, `index_up`, `index_down`, `op_avg`, `fut_bid_ask_counter`, `base`, `options`, `con_bid_ask_counter`) " +
+//                "VALUES ('349', 'ddd', 'ee', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'kkj', '0');";
+//
+//        PreparedStatement s = ( PreparedStatement ) connection.prepareStatement( query );
+//
+//        long startTime = System.currentTimeMillis( );
+//        s.execute( );
+//
+//        long endTime = System.currentTimeMillis( );
+//
+//        double duration = ( endTime - startTime );  //divide by 1000000 to get milliseconds
+//        System.out.println( duration / 1000 );
 
-        Statement stmt = ( Statement ) connection.createStatement( );
 
-        String query = "INSERT INTO `stocks`.`spx_daily` (`id`, `date`, `exp_name`, `open`, `high`, `low`, `close`, `con_up`, `con_down`, `index_up`, `index_down`, `op_avg`, `fut_bid_ask_counter`, `base`, `options`, `con_bid_ask_counter`) " +
-                "VALUES ('349', 'ddd', 'ee', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'kkj', '0');";
+        ExecutorService executor = Executors.newCachedThreadPool();
 
-        PreparedStatement s = ( PreparedStatement ) connection.prepareStatement( query );
+        executor.submit(new Runnable() {
+            @Override
+            public void run() {
 
-        long startTime = System.currentTimeMillis( );
-        s.execute( );
+                for (int i = 0; i < 30; i++) {
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("3");
+                }
 
-        long endTime = System.currentTimeMillis( );
+            }
+        });
 
-        double duration = ( endTime - startTime );  //divide by 1000000 to get milliseconds
-        System.out.println( duration / 1000 );
+        executor.submit(() -> {
+            String threadName = Thread.currentThread().getName();
+            System.out.println("Hello " + threadName);
+        });
 
-
+        System.out.println("None");
+        executor.shutdown();
     }
 
 }
