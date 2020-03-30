3320dc30814dd1e9e925838afe74fdb71b049a22
solomonsagiv <40433020+solomonsagiv@users.noreply.github.com>
2020-03-30 18:21:23 +0300

    Data base

--- src/main/java/api/Test.java
+++ src/main/java/api/Test.java
@@ -1,13 +1,25 @@
 package api;
 
 import org.jfree.data.time.Second;
+import serverObjects.indexObjects.Spx;
 
 import java.sql.SQLException;
+import java.util.Properties;
+import java.util.Random;
 
 public class Test {
 
     public static void main( String[] args ) throws SQLException {
-        System.out.println(new Second().getSecond());
+        Properties properties = new Properties();
+
+        properties.put( 4, "sds" );
+        properties.put( Props.IND, new Random(  ).nextDouble() * 100 );
+
     }
 
+}
+
+enum Props
+{
+    IND
 }
