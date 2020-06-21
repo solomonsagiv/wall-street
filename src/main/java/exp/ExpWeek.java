0bc577dad8b0ee6251a208901c3708a6ebc8fdea
sagiv <solomon.sagiv12@gmail.com>
2020-06-21 09:43:11 +0300

    Json data base

--- src/main/java/exp/ExpWeek.java
+++ src/main/java/exp/ExpWeek.java
@@ -2,11 +2,12 @@ package exp;
 
 import options.optionsCalcs.IOptionsCalcs;
 import serverObjects.BASE_CLIENT_OBJECT;
+import tws.TwsContractsEnum;
 
 public class ExpWeek extends Exp {
 
-    public ExpWeek(BASE_CLIENT_OBJECT client, ExpEnum expEnum, IOptionsCalcs iOptionsCalcs) {
-        super(client, expEnum, iOptionsCalcs);
+    public ExpWeek(BASE_CLIENT_OBJECT client, ExpEnum expEnum, TwsContractsEnum contractsEnum) {
+        super(client, expEnum, contractsEnum);
     }
 }
 
