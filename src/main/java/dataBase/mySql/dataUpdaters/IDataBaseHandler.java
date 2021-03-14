ad796c0c79099855299492c12e14ca9e5031a599
sagiv <solomon.sagiv12@gmail.com>
2021-03-14 09:47:49 +0200

    14.3.2021

--- src/main/java/dataBase/mySql/dataUpdaters/IDataBaseHandler.java
+++ src/main/java/dataBase/mySql/dataUpdaters/IDataBaseHandler.java
@@ -1,6 +1,13 @@
 package dataBase.mySql.dataUpdaters;
 
+import charts.myChart.MyTimeSeries;
+import dataBase.mySql.MySql;
 import serverObjects.BASE_CLIENT_OBJECT;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
 
 public abstract class IDataBaseHandler {
 
@@ -13,5 +20,88 @@ public abstract class IDataBaseHandler {
     public abstract void insertData( int sleep );
 
     public abstract void loadData();
+
+
+    protected void loadSerieDataAgg(String scheme, String table, MyTimeSeries timeSeries) {
+        String query = String.format("SELECT * FROM %s.%s WHERE time::date = now()::date;", scheme, table);
+        ResultSet rs = MySql.select(query);
+
+        double valAgg = 0;
+
+        while (true) {
+            try {
+                if (!rs.next()) break;
+                Timestamp timestamp = rs.getTimestamp(1);
+                double value = rs.getDouble(2);
+                valAgg += value;
+
+                timeSeries.add(timestamp.toLocalDateTime(), valAgg);
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+    }
+
+    protected void loadSerieData(String scheme, String table, MyTimeSeries timeSeries) {
+        String query = String.format("SELECT * FROM %s.%s WHERE time::date = now()::date;", scheme, table);
+
+        System.out.println(query);
+
+        ResultSet rs = MySql.select(query);
+        while (true) {
+            try {
+                if (!rs.next()) break;
+                Timestamp timestamp = rs.getTimestamp(1);
+                double value = rs.getDouble(2);
+                timeSeries.add(timestamp.toLocalDateTime(), value);
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+    }
+
+
+    protected void loadOpSerieData(String scheme, String table, List<Double> list ) {
+        String query = String.format("SELECT * FROM %s.%s WHERE time::date = now()::date;", scheme, table);
+
+        System.out.println(query);
+
+        ResultSet rs = MySql.select(query);
+        while (true) {
+            try {
+                if (!rs.next()) break;
+
+
+
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+    }
+
+
+    protected void insertListRetro(ArrayList<MyTimeStampObject> list, String scheme, String tableName) {
+        if (list.size() > 0) {
+
+            // Create the query
+            StringBuilder queryBuiler = new StringBuilder("INSERT INTO %s.%s (time, value) VALUES ");
+            int last_item_id = list.get(list.size() - 1).hashCode();
+            for (MyTimeStampObject row : list) {
+                queryBuiler.append(String.format("(cast('%s' as timestamp with time zone), %s)", row.getInstant(), row.getValue()));
+                if (row.hashCode() != last_item_id) {
+                    queryBuiler.append(",");
+                }
+            }
+            queryBuiler.append(";");
+
+            String q = String.format(queryBuiler.toString(), scheme, tableName);
+
+            // Insert
+            MySql.insert(q, true);
+
+            // Clear the list
+            list.clear();
+        }
+    }
 
 }
