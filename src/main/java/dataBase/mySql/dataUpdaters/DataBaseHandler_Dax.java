8ceb13c0e2826281588ea94964c36adb6ad5a3b8
Sagiv <solomon.sagiv12@gmail.com>
2021-05-31 11:47:26 +0300

    31.5.2021

--- src/main/java/dataBase/mySql/dataUpdaters/DataBaseHandler_Dax.java
+++ src/main/java/dataBase/mySql/dataUpdaters/DataBaseHandler_Dax.java
@@ -21,7 +21,6 @@ public class DataBaseHandler_Dax extends IDataBaseHandler {
     ArrayList<MyTimeStampObject> ind_counter_timestamp = new ArrayList<>();
     ArrayList<MyTimeStampObject> fut_races_timestamp = new ArrayList<>();
     ArrayList<MyTimeStampObject> baskets_timestamp = new ArrayList<>();
-    ArrayList<MyTimeStampObject> index_delta_timestamp = new ArrayList<>();
 
     double index_0 = 0;
     double fut_week_0 = 0;
@@ -31,7 +30,6 @@ public class DataBaseHandler_Dax extends IDataBaseHandler {
     double ind_counter_0;
     double fut_races_0 = 0;
     double baskets_0 = 0;
-    double index_delta_0 = 0;
 
     Exps exps;
 
@@ -119,16 +117,6 @@ public class DataBaseHandler_Dax extends IDataBaseHandler {
             baskets_timestamp.add(new MyTimeStampObject(Instant.now(), last_count));
         }
 
-        // Index delta
-        double index_delta = client.getStocksHandler().getDelta();
-
-        if (index_delta != index_delta_0) {
-            double last_count = index_delta - index_delta_0;
-            index_delta_0 = index_delta;
-            index_delta_timestamp.add(new MyTimeStampObject(Instant.now(), last_count));
-        }
-
-
         // --------------- Raw data --------------- //
         // Fut e1
 //        double fut_e1 = exps.getExp( ExpStrings.e1 ).getFuture( );
@@ -157,19 +145,14 @@ public class DataBaseHandler_Dax extends IDataBaseHandler {
         load_data_agg(MySql.Queries.get_serie(tablesNames.get(INDEX_RACES_TABLE)), client, null, INDEX_RACES_TYPE);
         load_data_agg(MySql.Queries.get_serie(tablesNames.get(FUT_RACES_TABLE)), client, null, FUT_RACES_TYPE);
 
-
-        // INDEX DELTA
-        load_data_agg(MySql.Queries.get_serie(tablesNames.get(INDEX_DELTA_TABLE)), client, null, INDEX_DELTA_TYPE);
     }
 
     @Override
     public void initTablesNames() {
         tablesNames.put(INDEX_TABLE, "data.dax_index");
-        tablesNames.put(INDEX_DELTA_TABLE, "data.dax_index_delta");
         tablesNames.put(INDEX_RACES_TABLE, "sagiv.dax_index_races_cdf");
         tablesNames.put(FUT_RACES_TABLE, "sagiv.dax_fut_races_cdf");
         tablesNames.put(BASKETS_TABLE, "data.dax_baskets_cdf");
-        tablesNames.put(INDEX_DELTA_TABLE, "data.dax_index_delta_cdf");
         tablesNames.put(FUT_WEEK_TABLE, "data.dax_fut_week");
         tablesNames.put(FUT_MONTH_TABLE, "data.dax_fut_month");
         tablesNames.put(FUT_Q1_TABLE, "data.dax_fut_gx1");
@@ -212,6 +195,5 @@ public class DataBaseHandler_Dax extends IDataBaseHandler {
         insertListRetro(ind_counter_timestamp, tablesNames.get(INDEX_RACES_TABLE));
         insertListRetro(fut_races_timestamp, tablesNames.get(FUT_RACES_TABLE));
         insertListRetro(baskets_timestamp, tablesNames.get(BASKETS_TABLE));
-        insertListRetro(index_delta_timestamp, tablesNames.get(INDEX_DELTA_TABLE));
     }
 }
