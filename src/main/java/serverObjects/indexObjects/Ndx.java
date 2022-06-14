2845974f584680d4636690a58f0bed3bf735f417
Sagiv <solomon.sagiv12@gmail.com>
2022-06-14 16:03:16 +0300

    14.6.22

--- src/main/java/serverObjects/indexObjects/Ndx.java
+++ src/main/java/serverObjects/indexObjects/Ndx.java
@@ -27,7 +27,7 @@ public class Ndx extends INDEX_CLIENT_OBJECT {
         setName("ndx");
         setId_name("ndx");
         setMySqlService(new MySqlService(this, new DataBaseHandler_Ndx(this)));
-        setBasketFinder_by_stocks(new BasketFinder_by_stocks(this, 90, 3));
+        setBasketFinder_by_stocks(new BasketFinder_by_stocks(this, 80, 3));
         setDdeHandler(new DDEHandler(this, new DDEReader_Ndx(this), new DDEWriter_Ndx(this)));
         setDataUpdaterService(new DataUpdaterService(this));
         roll();
